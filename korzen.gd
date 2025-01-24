6d210eec72d25f243cb21c8efa169f6d30a60d27
PieroganoReggiano <26669430+PieroganoReggiano@users.noreply.github.com>
2025-01-24 17:26:08 +0100

    added default świat i robienie rzeczy przez przycisk

--- korzen.gd
+++ korzen.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@onready var swiat_contaiener : Node = $"SwiatContainer"
+var default_swiat_scene = load("res://default_swiat.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +11,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func clear_children(node : Node) -> void:
+	var children = node.get_children()
+	for c in children:
+		node.remove_child(c)
+		c.queue_free()
+
+
+
+func reset_game() -> void:
+	clear_children(swiat_contaiener)
+	var swiat = default_swiat_scene.instantiate()
+	swiat_contaiener.add_child(swiat)
+	
