995eecf340a5c6ff27c138973e0627d8c0dca1ba
Gobral <umpalump97@gmail.com>
2025-01-25 00:14:07 +0100

    moved signals to arena1

--- skrypty/bombel.gd
+++ skrypty/bombel.gd
@@ -9,13 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
-	body.get_parent().remove_child(body)
-	add_child(body)
-
-
-func _on_body_entered(body: Node) -> void:
-	body.get_parent().remove_child(body)
-	add_child(body)
