fe39512d82fa9ac6ce041762f2cec44160d1f0b8
PieroganoReggiano <26669430+PieroganoReggiano@users.noreply.github.com>
2025-01-25 21:23:31 +0100

    reversed speed

--- parkourowiec.gd
+++ parkourowiec.gd
@@ -109,7 +109,7 @@ func _physics_process(delta: float) -> void:
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
 	var speed = BASE_SPEED
-	if Input.is_action_pressed("move_speed"):
+	if not Input.is_action_pressed("move_speed"):
 		speed *= SPEED_MULTIPLIER
 	elif Input.is_action_pressed("move_crouch"):
 		speed *= CROUCH_MULTIPLIER
