88abe353a4799809932171ed3e83d7d36bbe0c3b
nkp123 <4711143+nkp123@users.noreply.github.com>
2025-01-25 13:52:35 +0100

    Sticky bubble wip

--- sceny/arena_1.gd
+++ sceny/arena_1.gd
@@ -9,11 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_bombel_body_entered(body: Node) -> void:
-	print('ender 3 body') # Replace with function body.
-
-
-func _on_bombel_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
-	print('ender 3 shape') # Replace with function body.
