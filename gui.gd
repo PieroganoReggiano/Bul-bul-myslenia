888edb7ccd6656869ccba4c5d840c07dd8529a09
PieroganoReggiano <26669430+PieroganoReggiano@users.noreply.github.com>
2025-01-25 19:48:31 +0100

    added fajne menu z bąbelkami

--- gui.gd
+++ gui.gd
@@ -4,19 +4,31 @@ extends Control
 @onready var korzen : Korzen = $".."
 @onready var menu = $Menu
 
-# Called when the node enters the scene tree for the first time.
+@onready var button_continue = $"Menu/Panel/ButtonContinue"
+@onready var button_new_game = $"Menu/Panel/ButtonPlay"
+@onready var nazwa = $"Menu/Panel/Nazwa"
+
 func _ready() -> void:
-	pass # Replace with function body.
+	refresh()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("go_back") and not menu.visible:
 		make_pause()
 
 
+func refresh() -> void:
+	var is_game : bool = korzen.is_game()
+	button_continue.visible = is_game
+	nazwa.visible = not is_game
+	button_new_game.text = \
+		"New game" if is_game else "Play"
+	button_new_game.refresh()
+
+
 func make_pause() -> void:
 	korzen.go_to_menu()
+	refresh()
 
 
 func drop_game() -> void:
@@ -24,6 +36,15 @@ func drop_game() -> void:
 	korzen.go_to_menu()
 
 
-func play() -> void:
+func new_game() -> void:
+	korzen.reset_game()
+	korzen.go_to_game()
+
+
+func continue_game() -> void:
 	korzen.play()
 	korzen.go_to_game()
+
+
+func quit() -> void:
+	get_tree().quit()
--- korzen.gd
+++ korzen.gd
@@ -10,14 +10,9 @@ var current_player : Parkourowiec
 
 @export var sensitivity : float = 2.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func is_game() -> bool:
+	return swiat_container and swiat_container.get_child_count() > 0
 
 
 func go_to_menu() -> void:
@@ -71,7 +66,7 @@ func reset_game() -> void:
 
 
 func play() -> void:
-	if swiat_container.get_child_count() < 1:
+	if not is_game():
 		reset_game()
 	else:
 		# TODO unpause
